ce7a2d931513971b0bcfe1419d71fa6ad7661640
Jason Trill <jason@jasontrill.com>
2013-06-17 01:20:31 -0700

    Transition to tasks/pure-place.js

--- tasks/pure-place.js
+++ tasks/pure-place.js
@@ -83,7 +83,7 @@ module.exports = function(grunt) {
           return next();
         }
 
-        grunt.file.copy(path.resolve('../lib/_grid-functions.scss'), 'scss/');
+        grunt.file.copy('../lib/_grid-functions.scss', 'scss/');
 
         // Master file that @imports all of the modules
         var placeholders_dest = "scss/_pure-placeholders.scss",
